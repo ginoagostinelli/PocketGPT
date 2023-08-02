b21f7afc4d7247d3ce775009559a38d53c526b99
Gino Agostinelli <ginoagostinelli5@gmail.com>
2023-08-01 23:55:50 -0300

    add cuda processing and dataloader

--- model/trainer.py
+++ model/trainer.py
@@ -3,6 +3,7 @@ import os
 import torch
 import numpy as np
 import torch.nn as nn
+from torch.utils.data import DataLoader, Dataset
 
 # config
 batch_size = 32
@@ -16,8 +17,22 @@ save_checkpoint = True
 resume_from_checkpoint = False
 dataset = 'cornell_movie_dialogs'
 dataset_path = os.path.join('data', dataset)
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
 # ------------
 output_dir = os.path.join(os.path.dirname(__file__), 'output')
+
+class Dataset(Dataset):
+    def __init__(self, dataset_path):
+        self.dataset = np.memmap(dataset_path, dtype=np.uint16, mode='r')
+
+    def __len__(self):
+        return len(self.dataset) - context_size
+
+    def __getitem__(self, idx):
+        x = torch.from_numpy(self.dataset[idx: idx + context_size].astype(np.int64))
+        y = torch.from_numpy(self.dataset[idx + 1: idx + 1 + context_size].astype(np.int64))
+        return x, y
+
 
 class Trainer:
 
@@ -27,30 +42,33 @@ class Trainer:
             raise RuntimeError('"Trainer" requires a model to be specified')
         self.model = model
 
-        # TODO: add a data loader 
-        self.train_dataset = np.memmap(os.path.join(dataset_path, 'train.bin'), dtype=np.uint16, mode='r')
-        self.val_dataset = np.memmap(os.path.join(dataset_path, 'val.bin'), dtype=np.uint16, mode='r')
+        self.train_dataset = Dataset(os.path.join(dataset_path, 'train.bin'))
+        self.val_dataset = Dataset(os.path.join(dataset_path, 'val.bin'))
 
-
-    def get_batch(self, split):
-        data = self.train_dataset if split == 'train' else self.val_dataset
-        ix = torch.randint(len(data) - context_size, (batch_size,))
-        x = torch.stack([torch.from_numpy((data[i:i+context_size]).astype(np.int64)) for i in ix])
-        y = torch.stack([torch.from_numpy((data[i+1:i+1+context_size]).astype(np.int64)) for i in ix])
-        return x, y
-    
+    @staticmethod
+    def get_dataloader(dataset, shuffle=True) -> DataLoader:
+        return DataLoader(
+            dataset,
+            batch_size=batch_size,
+            shuffle=shuffle,
+            pin_memory=(device == "cuda"),
+        )
 
     @torch.no_grad()
-    def predict_loss(self):
+    def predict_loss(self) -> dict:
         output = {}
         self.model.eval()
-        for split in ['train', 'val']:
+
+        for split, dataloader in [("train", self.get_dataloader(self.train_dataset, shuffle=False)),
+                                   ("val", self.get_dataloader(self.val_dataset, shuffle=False))]:
+            data = iter(dataloader)
             losses = torch.zeros(predict_iters)
             for k in range(predict_iters):
-                X, Y = self.get_batch(split)
+                X, Y = next(data)
+                X, Y = X.to(device), Y.to(device)
                 loss, _ = self.model(X, Y)
-                losses[k] = loss.item()
-            output[split] = losses.mean()
+                losses[k] += loss.item()
+            output[split] = losses.mean().item()
         self.model.train()
         return output
 
@@ -60,38 +78,45 @@ class Trainer:
         os.makedirs(output_dir, exist_ok=True)
         torch.save(self.model.state_dict(), os.path.join(output_dir, 'model.pt'))
 
-
     def train(self):
         if resume_from_checkpoint:
-            model_path = os.path.join(output_dir, 'model.pt')
+            model_path = os.path.join(output_dir, "model.pt")
             self.model.from_pretrained(model_path)
-    
+
+        self.model.to(device)
         optimizer = torch.optim.AdamW(self.model.parameters(), lr=learning_rate)
 
-        for iter in range(batch_iterations):
+        dataloader = self.get_dataloader(self.train_dataset)
+        data = iter(dataloader)
 
-            if iter % eval_interval == 0 or iter == batch_iterations - 1:
+        for iteration in range(batch_iterations):
+            if iteration % eval_interval == 0 or iteration == batch_iterations - 1:
                 predicted_loss = self.predict_loss()
-                print(f"STEP {iter} --> Training loss: {predicted_loss['train']:.4f} || Validation loss: {predicted_loss['val']:.4f}")
+                print(
+                    f"STEP {iteration} --> Training loss: {predicted_loss['train']:.4f} || Validation loss: {predicted_loss['val']:.4f}"
+                )
 
-            xb, yb = self.get_batch('train')
-
+            xb, yb = next(data)
+            xb, yb = xb.to(device), yb.to(device)
             loss, _ = self.model(xb, yb)
-            optimizer.zero_grad(set_to_none=True)
-            
+            optimizer.zero_grad()
             loss.backward()
             optimizer.step()
-        
+
         if save_checkpoint:
-            self.save_model(output_dir)
+            with torch.no_grad():
+                self.save_model(output_dir)
 
 
 def main():
     args = ModelArgs()
     model = GPT(args)
     trainer = Trainer(model)
+
+    print(f'Device: {device}')
+    # TODO: add model parameters
+    
     trainer.train()
-
-
+    
 if __name__ == "__main__":    
     main()
